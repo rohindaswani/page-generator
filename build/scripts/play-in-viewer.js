a65c30e372fab88e6b81de16b246ebb8997986fb
rohindaswani <rohindaswani@gmail.com>
2017-10-24 13:49:06 -0400

    - updates for 2244

---
+++ build/scripts/play-in-viewer.js
@@ -0,0 +1,11 @@
+function playInViewer(node) {
+  if (typeof node === "string") node = document.querySelector(node);
+  function play() {
+    var top = node.getBoundingClientRect().top;
+    if( top < 550 ) {
+      node.play();
+    }
+  }
+  window.addEventListener('scroll', function(event) { play(); }, false);
+  play()
+}
