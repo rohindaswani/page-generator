b52c829411c14a5001370a55f3d4d807a327e3ea
rohindaswani <rohindaswani@gmail.com>
2017-11-21 12:28:09 -0800

    - files for 2191 - New script for pinning divs

---
+++ build/scripts/pin-div-with-viewer.js
@@ -0,0 +1,21 @@
+function pinTheDiv(node, minScroll, maxScroll) {
+  if (typeof node === "string") node = document.querySelector(node);
+  var scale = node.offsetTop / minScroll;
+  minScroll = node.offsetTop;
+  maxScroll *= scale;
+  function animator() {
+    var scroll = window.pageYOffset;
+    if (scroll < minScroll) {
+      node.style.position = "absolute";
+      node.style.top = minScroll + "px";
+    } else if (scroll < maxScroll - node.clientHeight) {
+      node.style.top = 0;
+      node.style.position = "fixed";
+    } else {
+      node.style.position = "absolute";
+      node.style.top = maxScroll - node.clientHeight + "px";
+    }
+  }
+  window.addEventListener('scroll', function(event) { window.requestAnimationFrame(animator); }, false);
+  animator();
+}
